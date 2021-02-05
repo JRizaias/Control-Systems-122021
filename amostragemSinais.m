57266620523bc7d2b4c8ee6dcaaf09fd809b7142
Izaias A S Junior <53841259+JRizaias@users.noreply.github.com>
2021-02-04 23:35:12 -0300

    Upload 01
    Upload 01 file for sampling continuous signals.

---
+++ amostragemSinais.m
@@ -0,0 +1,56 @@
+clear all
+clc
+Tfinal=0.1;
+Fs=1500;                    %Frequencia de amostragem               
+Ts=1/Fs;                    %Periodo de amostragem             
+MfreqSinal=800;             %Limite superior para maior frequncia no espectro
+                            %de frequencia 
+passo=(10^(-6));            %Passo no vetor de tempo continuo
+
+t=0:passo:Tfinal;           %Vetor no tempo continuo           
+x =sin(50*2*pi*t)+sin(100*2*pi*t)+sin(500*2*pi*t); %Sinal a ser amostrado
+
+i=1;
+for v = 0:+Ts:Tfinal        %Amostragem de sinal
+   txs(i)= v;               %Vetor no tempo discreto                                           
+   yxs(i)=sin(50*2*pi*v)+sin(100*2*pi*v)+sin(500*2*pi*v);     
+   i=i+1;
+end
+
+%------------------espetro de frequencia do sinal amostrado-----------------------------------
+yfft = fft(yxs);     
+% f = (0:length(xfft)-1)*MfreqSinal/length(xfft);
+nfft = length(yxs);                         
+fshift = (-nfft/2:nfft/2-1)*(2*MfreqSinal/nfft);
+yshift = fftshift(yfft);
+
+%------------------Geraçao de graficos-----------------------------------
+figure(1)
+subplot(3,1,1);
+
+plot(t,x, 'r');
+title('Sinal continuo no tempo');
+xlabel('time');
+%------------------------
+subplot(3,1,2);
+
+%plot(n,xs, '*');
+        %ou
+%plot(txs,yxs, 'o');
+        %ou
+stem(txs,yxs);
+title('Sinal discreto no tempo (Amostragem)');
+xlabel('time');
+%------------------------
+subplot(3,1,3);
+stairs(txs,yxs);
+hold on 
+%stem(n,xs);
+plot(t,x)
+title('Sinal reconstruido');
+xlabel('time');
+
+%------------------------
+figure(2)
+plot(fshift,abs(yshift))
+title('Magnitude')
